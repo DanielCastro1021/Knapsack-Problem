963b34a793d38e465366c424340d0a7ec0f0c84a
Daniel Castro <danielcastrofafe@gmail.com>
2021-05-17 09:02:25 +0100

    Updated script

--- knapsack.py
+++ knapsack.py
@@ -4,22 +4,18 @@ from tabulate import tabulate
 from ortools.algorithms import pywrapknapsack_solver
 
 
-class TimeLimit(Exception):
-    pass
-
-
 class KnapsackSolver:
 
-    def __init__(self, capacity, values, weights, method, time_limit):
+    def __init__(self, capacity, values, weights, solver, time_limit):
         self.capacity, self.values, self.weights, self.time_limit = int(
             capacity), values, weights, time_limit
 
-        if method == 1:
+        if solver == "dynamic_programming_solver":
             self.solver = self.dynamic_programming_solver
-        elif method == 2:
+        elif solver == "ortools_solver":
             self.solver = self.ortools_solver
         else:
-            raise Exception("Method not found.")
+            raise Exception("Solver not found.")
 
     def solve(self):
         return self.solver()
@@ -56,14 +52,13 @@ class KnapsackSolver:
         solver = pywrapknapsack_solver.KnapsackSolver(
             pywrapknapsack_solver.KnapsackSolver.
             KNAPSACK_MULTIDIMENSION_BRANCH_AND_BOUND_SOLVER, 'KnapsackExample')
+        solver.set_time_limit(self.time_limit)
 
         start_time = time.perf_counter()
-
         solver.Init(self.values, [self.weights], [self.capacity])
-        solver.set_time_limit(self.time_limit)
         solution = solver.Solve()
-
         time_to_solve = round(time.perf_counter()-start_time, 3)
+
         return solution, format(time_to_solve, '.3f')
 
 
@@ -73,7 +68,7 @@ def get_optimum_solution(file):
         return int(lines[0].strip().split()[0])
 
 
-def get_solution(file, method, time_limit):
+def get_solution(file, solver, time_limit):
     capacity, values, weights = 0, [], []
     with open(file, "r") as f:
         lines = f.readlines()
@@ -86,24 +81,24 @@ def get_solution(file, method, time_limit):
             except:
                 pass
     knapsack_solver = KnapsackSolver(
-        capacity, values, weights, method, time_limit)
+        capacity, values, weights, solver, time_limit)
 
     return knapsack_solver.solve()
 
 
-def main(folder, current_dataset, method, time_limit):
+def main(folder, current_dataset, solver, time_limit):
     files = os.listdir(os.path.join(folder, current_dataset))
     results = []
-    for i in range(len(files)):
+    for file in files:
         optimum_solution = get_optimum_solution(os.path.join(
-            folder, current_dataset+"-optimum", files[i]))
+            folder, current_dataset+"-optimum", file))
 
         solution, time = get_solution(os.path.join(
-            folder, current_dataset, files[i]), method, time_limit)
+            folder, current_dataset, file), solver, time_limit)
 
         gap = round(((optimum_solution-solution)/optimum_solution)*100, 2)
 
-        results.append([files[i],
+        results.append([file,
                        optimum_solution, solution, gap, time])
 
     print(tabulate(results, headers=[
@@ -111,8 +106,11 @@ def main(folder, current_dataset, method, time_limit):
 
 
 if __name__ == "__main__":
-    folder, datasets = "KP-instances", {0: "low-dimensional", 1: "large_scale"}
+    folder, datasets, solvers = "KP-instances", {
+        1: "low-dimensional", 2: "large_scale"}, {1: "dynamic_programming_solver",  2: "ortools_solver"}
+
     current_dataset = datasets.get(1)
-    method = 1
+    solver = solvers.get(2)
     time_limit = 10.00
-    main(folder, current_dataset, method, time_limit)
+
+    main(folder, current_dataset, solver, time_limit)
